2482b0610efd4014e47b8542b0192789e8f5640a
Brian Feister <bfeister@salesforce.com>
2025-08-06 07:46:47 -0600

    Fix URL parameter handling by moving logic to React component

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,7 +4,4 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
  */
 
-// Import the map script to handle query parameters
-import "./src/map-script.js"
-
 // You can delete this file if you're not using it
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,34 @@ import Seo from "../components/seo"
 const IndexPage = () => {
   const googleApiKey = process.env.GATSBY_GOOGLE_API_KEY
   const defaultAddress = "New York, NY"
+  
+  // Get address from URL parameters
+  const [address, setAddress] = React.useState(defaultAddress)
+  const [iframeSrc, setIframeSrc] = React.useState("")
+  
+  React.useEffect(() => {
+    // Get address from URL query parameter
+    const urlParams = new URLSearchParams(window.location.search)
+    const addressFromUrl = urlParams.get("address") || defaultAddress
+    
+    console.log("Address from URL:", addressFromUrl)
+    setAddress(addressFromUrl)
+    
+    // Update iframe src
+    if (googleApiKey && googleApiKey !== "YOUR_API_KEY_HERE") {
+      const newSrc = `https://www.google.com/maps/embed/v1/place?key=${googleApiKey}&q=${encodeURIComponent(addressFromUrl)}`
+      setIframeSrc(newSrc)
+      console.log("Updated iframe src to:", newSrc)
+    } else {
+      console.log("API key not available or invalid")
+    }
+  }, [googleApiKey])
 
   return (
     <Layout>
       <section style={{ width: "100%" }}>
         <h1 style={{ fontSize: "2rem", marginBottom: "1rem" }}>
-          Map Page - <span id="address-display">{defaultAddress}</span>
+          Map Page - <span id="address-display">{address}</span>
         </h1>
         <div
           id="embed-map"
@@ -23,7 +45,7 @@ const IndexPage = () => {
         >
           <iframe
             id="map-iframe"
-            src={`https://www.google.com/maps/embed/v1/place?key=${
+            src={iframeSrc || `https://www.google.com/maps/embed/v1/place?key=${
               googleApiKey || "YOUR_API_KEY_HERE"
             }&q=${encodeURIComponent(defaultAddress)}`}
             width="100%"
